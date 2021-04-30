8bfd6a97f16b72dabe0e637e0d3b1578cfb6d8df
Amro dandashli <amrdandashli@gmail.com>
2021-04-30 15:50:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 // Code your solution here
 const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
 1
+const findMatching = (drivers, string) => drivers.filter(driver =>  driver.toLowerCase() === string.toLowerCase() )
