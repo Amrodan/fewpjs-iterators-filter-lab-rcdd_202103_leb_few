44765d776f6c229d5227c5fc7274219d71337531
Amro dandashli <amrdandashli@gmail.com>
2021-04-30 16:03:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,5 @@
 const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
 1
 const findMatching = (drivers, string) => drivers.filter(driver =>  driver.toLowerCase() === string.toLowerCase() )
+
+const fuzzyMatch = (drivers,string)=> drivers.filter(driver=> driver.startsWith(string)=>true;)
